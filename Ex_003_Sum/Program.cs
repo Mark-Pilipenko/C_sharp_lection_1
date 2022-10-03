0abe5986b2872f5284e0efbda2766cbbcca96e3b
Mark <kalmark@mail.ru>
2022-10-03 19:06:51 +0300

    Add random

--- Ex_003_Sum/Program.cs
+++ Ex_003_Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);  // 1,2,3,...9
+Console.Write(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write(" + ");
+Console.Write(numberB);
 int result = numberA + numberB;
+Console.Write(" = ");
 Console.WriteLine(result);
