2dc20502e63f433005b2a4edfe7025f2509ba250
Mark <kalmark@mail.ru>
2022-10-03 19:30:10 +0300

    Add Ex_005_If_Else project

---
+++ Ex_005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("What is your name?");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hurrah! Congratulations! This is Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+    Console.WriteLine("!");
+}
